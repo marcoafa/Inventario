323fd35b755243ddcd9e015624116ba484a79110
Marco <marco.fuentes.isc@gmail.com>
2016-10-04 11:18:57 -0700

    Clase hardware con componentes agregada

---
+++ Inventario/Models/hardwareWithComnponents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventario.Models
+{
+    public class hardwareWithComnponents
+    {
+        public Hardware hardware{get;set;}
+        public List<Hardware> components{get;set;}
+
+    }
+}
